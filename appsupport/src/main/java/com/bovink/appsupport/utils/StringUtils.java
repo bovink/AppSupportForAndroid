f3f54fa6d5ce92c1a5ee86d659b53a4323c03538
bovink@sina.com <261512335wen>
2017-11-06 15:29:32 +0800

    字符串添加判断是否是手机号码

--- appsupport/src/main/java/com/bovink/appsupport/utils/StringUtils.java
+++ appsupport/src/main/java/com/bovink/appsupport/utils/StringUtils.java
@@ -21,4 +21,24 @@ public class StringUtils {
         String m = s.substring(10, 15);
         return year + "年" + month + "月" + day + "日" + " " + m;
     }
+
+    /**
+     * 判断电话号码是否符合规范
+     *
+     * @param number 电话号码
+     * @return 电话号码是否符合规范
+     */
+    public static boolean isMobileNumber(String number) {
+        /*
+         * 移动：134、135、136、137、138、139、150、151、157(TD)、158、159、187、188
+		 * 联通：130、131、132、152、155、156、185、186 电信：133、153、180、189、（1349卫通）
+		 * 总结起来就是第一位必定为1，第二位必定为3或5或8，其他位置的可以为0-9
+		 */
+        String telRegex = "[1][3578]\\d{9}";// "[1]"代表第1位为数字1，"[358]"代表第二位可以为3、5、8中的一个，"\\d{9}"代表后面是可以是0～9的数字，有9位。
+        if (android.text.TextUtils.isEmpty(number)) {
+            return false;
+        } else {
+            return number.matches(telRegex);
+        }
+    }
 }
