b8b27d1db69065134b07a371820198a5a09b9383
bovink <bovink@sina.com>
2018-01-14 16:31:43 +0800

    删除旧控件。

--- app/src/main/java/com/bovink/appsupportforandroid/MainActivity.java
+++ app/src/main/java/com/bovink/appsupportforandroid/MainActivity.java
@@ -1,9 +1,7 @@
 package com.bovink.appsupportforandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.bovink.appsupport.widget.view.GradientDrawableTextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        GradientDrawableTextView testTextView = (GradientDrawableTextView) findViewById(R.id.tv_test);
-        testTextView.setClickable(true);
     }
 }
